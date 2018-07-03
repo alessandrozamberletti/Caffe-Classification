1835da86f75240756b4c56535e9e57802f5a454a
Alessandro Zamberletti <alessandro.zamberletti@gmail.com>
2018-07-03 17:49:58 +0200

    * created nb project

--- main.cpp
+++ main.cpp
@@ -1,8 +1,13 @@
-#include <cstdlib>
+#define CPU_ONLY
+#define USE_OPENCV
+
+#include "caffe/net.hpp"
 
 using namespace std;
 
 int main(int argc, char** argv) {
+    
+    caffe::Net<float> cnn();
 
     return 0;
 }
